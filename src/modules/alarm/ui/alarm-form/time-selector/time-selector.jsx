9d201b6ba49ef9883e9c2a3353ceb13b23a7fc49
Александр Кошелев <alexdevelwork@gmail.com>
2024-10-21 22:54:57 +0500

    fix: рефакторинг TimeSelector

--- src/modules/alarm/ui/alarm-form/time-selector/time-selector.jsx
+++ src/modules/alarm/ui/alarm-form/time-selector/time-selector.jsx
@@ -1,61 +1,52 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import {
-  addLeadingZero,
-  createTimeArray,
-  minutesToHoursAndMinutes,
-  removeLeadingZero,
-} from "./utils";
+import { rangeHoursAndMinutes, minutesToHoursAndMinutes } from "./utils";
 
 export const TimeSelector = ({ triggerTimeMinutes, setTriggerTimeMinutes }) => {
   const { hours: currentHour, remainingMinutes: currentMinutes } =
     minutesToHoursAndMinutes(triggerTimeMinutes);
 
-  const [selectorsTime, setSelectorsTime] = useState({
-    h: currentHour,
-    m: currentMinutes,
-  });
+  const [hour, setHour] = useState(currentHour);
+  const [minute, setMinute] = useState(currentMinutes);
 
   useEffect(() => {
-    const minutes = selectorsTime.h * 60 + selectorsTime.m;
+    const minutes = hour * 60 + minute;
     setTriggerTimeMinutes(minutes);
-  }, [selectorsTime, setTriggerTimeMinutes]);
+  }, [hour, minute, setTriggerTimeMinutes]);
 
-  const hours = createTimeArray(24);
-  const minutes = createTimeArray(60);
+  const hours = rangeHoursAndMinutes(24);
+  const minutes = rangeHoursAndMinutes(60);
+
+  function handleChangeHour(e) {
+    setHour(() => parseInt(e.target.value));
+  }
+
+  function handleChangeMinute(e) {
+    setMinute(() => parseInt(e.target.value));
+  }
 
   return (
     <div className="flex justify-center items-center gap-4 mb-10">
       <select
-        value={addLeadingZero(selectorsTime.h)}
-        onChange={(e) =>
-          setSelectorsTime((prevState) => ({
-            ...prevState,
-            h: removeLeadingZero(e.target.value),
-          }))
-        }
+        value={hour}
+        onChange={handleChangeHour}
         className="bg-gray-800 p-2 text-2xl rounded focus:outline-none focus:ring-2 focus:ring-gray-500 custom-scrollbar"
       >
         {hours.map((h) => (
           <option key={h} value={h}>
-            {h}
+            {h.toString().padStart(2, "0")}
           </option>
         ))}
       </select>
       <span>:</span>
       <select
-        value={addLeadingZero(selectorsTime.m)}
-        onChange={(e) =>
-          setSelectorsTime((prevState) => ({
-            ...prevState,
-            m: removeLeadingZero(e.target.value),
-          }))
-        }
+        value={minute}
+        onChange={handleChangeMinute}
         className="bg-gray-800 p-2 text-2xl rounded focus:outline-none focus:ring-2 focus:ring-gray-500 custom-scrollbar"
       >
         {minutes.map((m) => (
           <option className="max-h-1" key={m} value={m}>
-            {m}
+            {m.toString().padStart(2, "0")}
           </option>
         ))}
       </select>
--- src/modules/alarm/ui/alarm-form/time-selector/utils.js
+++ src/modules/alarm/ui/alarm-form/time-selector/utils.js
@@ -5,8 +5,8 @@
  * @param {number} length - Длина массива
  * @returns {string[]} Массив строк с двумя цифрами
  */
-export const createTimeArray = (length) => {
-  return Array.from({ length }, (_, i) => i.toString().padStart(2, "0"));
+export const rangeHoursAndMinutes = (length) => {
+  return Array.from({ length }, (_, i) => i);
 };
 
 /**
@@ -19,24 +19,4 @@ export function minutesToHoursAndMinutes(minutes) {
   let hours = Math.floor(minutes / 60);
   let remainingMinutes = minutes % 60;
   return { hours, remainingMinutes };
-}
-
-/**
- * Добавляет ведущий ноль к числу, если оно меньше 10.
- *
- * @param {number} num - Число для форматирования
- * @returns {number} Строка с двумя цифрами или само число
- */
-export function addLeadingZero(num) {
-  return num < 10 ? "0" + num : String(num);
-}
-
-/**
- * Удаляет ведущий ноль из строки и возвращает число.
- *
- * @param {string} str - Число для обработки
- * @returns {number} Обработанное число без ведущего нуля
- */
-export function removeLeadingZero(str) {
-  return Number(str.toString().replace(/^0+/, ""));
 }
