979f2564e8abb1139a6f9e20fe6e2c09aab9fcab
Александр Кошелев <alexdevelwork@gmail.com>
2024-10-27 12:59:54 +0500

    fix: добавил типизацию

--- src/common/lib/date-time/get-next-day-of-week-number.js
+++ src/common/lib/date-time/get-next-day-of-week-number.js
@@ -1,3 +1,9 @@
+/**
+ * Возвращает номер следующего дня недели.
+ *
+ * @returns {number} Номер дня недели (0 - воскресенье, 1 - понедельник и т.д.)
+ */
+
 export function getNextDayOfWeekNumber() {
   const today = new Date();
   const dayOfWeek = today.getDay();
